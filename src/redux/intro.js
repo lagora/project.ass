bb8358baa2b11b38acb65763473601b0d16ec832
lagora <lagora.franck@gmail.com>
2017-07-31 01:48:37 +0200

    intro redux bundle

---
+++ src/redux/intro.js
@@ -0,0 +1,30 @@
+import {
+    blank, color, frame, rect, 
+    reducer, soapLogo, 
+    text
+} from '../helpers';
+
+const state = {};
+
+const INTRO_INIT = 'INTRO_INIT';
+
+export const types = {INTRO_INIT};
+
+export const init = actions => (dispatch, getState) => {
+    return dispatch({
+        type: INTRO_INIT,
+        obj: [
+            {
+                draw: ({ctx, delta, gfx, time}) => soapLogo(gfx)({time}),
+                update: ({actions, delta, state, time}) => time > 5000 && actions.game.setState('ttls'),
+                state: 'intro',
+            }
+        ]
+    });
+};
+
+export const actions = {init};
+
+export const mapping = {};
+
+export default reducer(mapping, state);
